2407c8bcfbe6e62afb9045a50764f4cebbddb6f9
Dave Cramer <davec@postgresintl.com>
2017-02-17 07:05:46 -0500

    first cut at working version

--- src/main/java/com/postgresintl/logicaldecoding/App.java
+++ src/main/java/com/postgresintl/logicaldecoding/App.java
@@ -1,13 +1,194 @@
 package com.postgresintl.logicaldecoding;
 
+import java.nio.ByteBuffer;
+import java.sql.*;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.postgresql.PGConnection;
+import org.postgresql.PGProperty;
+import org.postgresql.core.BaseConnection;
+import org.postgresql.core.ServerVersion;
+import org.postgresql.replication.LogSequenceNumber;
+import org.postgresql.replication.PGReplicationStream;
+
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private final static String SLOT_NAME="slot";
+
+    Connection connection;
+    Connection replicationConnection;
+
+    private static String toString(ByteBuffer buffer) {
+        int offset = buffer.arrayOffset();
+        byte[] source = buffer.array();
+        int length = source.length - offset;
+
+        return new String(source, offset, length);
+    }
+
+    public void createConnection()
+    {
+        try
+        {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/test","davec","");
+        }
+        catch (SQLException ex)
+        {
+
+        }
+
+    }
+    public void createLogicalReplicationSlot(String slotName, String outputPlugin ) throws InterruptedException, SQLException, TimeoutException
+    {
+        //drop previos slot
+        dropReplicationSlot(connection, slotName);
+
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM pg_create_logical_replication_slot(?, ?)") )
+        {
+
+            preparedStatement.setString(1, slotName);
+            preparedStatement.setString(2, outputPlugin);
+            try (ResultSet rs = preparedStatement.executeQuery())
+            {
+                while (rs.next())
+                {
+                    System.out.println("Slot Name: " + rs.getString(1));
+                    System.out.println("Xlog Position: " + rs.getString(2));
+                }
+            }
+
+        }
+    }
+
+    public void dropReplicationSlot(Connection connection, String slotName)
+            throws SQLException, InterruptedException, TimeoutException
+    {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                        "select pg_terminate_backend(active_pid) from pg_replication_slots "
+                                + "where active = true and slot_name = ?"))
+        {
+            preparedStatement.setString(1, slotName);
+            preparedStatement.execute();
+        }
+
+        waitStopReplicationSlot(connection, slotName);
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("select pg_drop_replication_slot(slot_name) "
+                            + "from pg_replication_slots where slot_name = ?")) {
+            preparedStatement.setString(1, slotName);
+            preparedStatement.execute();
+        }
+    }
+
+    public  boolean isReplicationSlotActive(Connection connection, String slotName)
+            throws SQLException
+    {
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("select active from pg_replication_slots where slot_name = ?")){
+            preparedStatement.setString(1, slotName);
+            try (ResultSet rs = preparedStatement.executeQuery())
+            {
+                return rs.next() && rs.getBoolean(1);
+            }
+        }
+    }
+
+    private  void waitStopReplicationSlot(Connection connection, String slotName)
+            throws InterruptedException, TimeoutException, SQLException
+    {
+        long startWaitTime = System.currentTimeMillis();
+        boolean stillActive;
+        long timeInWait = 0;
+
+        do {
+            stillActive = isReplicationSlotActive(connection, slotName);
+            if (stillActive) {
+                TimeUnit.MILLISECONDS.sleep(100L);
+                timeInWait = System.currentTimeMillis() - startWaitTime;
+            }
+        } while (stillActive && timeInWait <= 30000);
+
+        if (stillActive) {
+            throw new TimeoutException("Wait stop replication slot " + timeInWait + " timeout occurs");
+        }
+    }
+    public void receiveChangesOccursBeforStartReplication() throws Exception {
+        PGConnection pgConnection = (PGConnection) replicationConnection;
+
+        LogSequenceNumber lsn = getCurrentLSN();
+
+        Statement st = connection.createStatement();
+        st.execute("insert into test_logic_table(name) values('previous value')");
+        st.close();
+
+        PGReplicationStream stream =
+                pgConnection
+                        .getReplicationAPI()
+                        .replicationStream()
+                        .logical()
+                        .withSlotName(SLOT_NAME)
+                        .withStartPosition(lsn)
+                        .withSlotOption("include-xids", false)
+                        .start();
+        ByteBuffer buffer;
+        for (int i=0; i<3; i++)
+        {
+            buffer = stream.read();
+            System.out.println( toString(buffer));
+        }
+        stream.close();
+    }
+
+    private LogSequenceNumber getCurrentLSN() throws SQLException
+    {
+        try (Statement st = connection.createStatement())
+        {
+            try (ResultSet rs = st.executeQuery("select "
+                    + (((BaseConnection) connection).haveMinimumServerVersion(ServerVersion.v10)
+                    ? "pg_current_wal_location()" : "pg_current_xlog_location()"))) {
+
+                if (rs.next()) {
+                    String lsn = rs.getString(1);
+                    return LogSequenceNumber.valueOf(lsn);
+                } else {
+                    return LogSequenceNumber.INVALID_LSN;
+                }
+            }
+        }
+    }
+
+    private void openReplicationConnection() throws Exception {
+        Properties properties = new Properties();
+        PGProperty.ASSUME_MIN_SERVER_VERSION.set(properties, "9.4");
+        PGProperty.REPLICATION.set(properties, "database");
+        replicationConnection = DriverManager.getConnection("jdbc:postgresql://localhost/test",properties);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.createConnection();
+        try {
+            app.createLogicalReplicationSlot(SLOT_NAME, "test_decoding");
+            app.openReplicationConnection();
+            app.receiveChangesOccursBeforStartReplication();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
